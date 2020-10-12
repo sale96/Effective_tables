e7635900bf4b26d941844fa0f1b1a83b3218652a
Aleksandar <aca801@gmail.com>
2020-10-12 23:53:37 +0200

    Add Card Log entity

---
+++ server/EffectiveTables/EffectiveTables.Domain/Entities/CardLog.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EffectiveTables.Domain.Entities
+{
+    public class CardLog : BaseEntity
+    {
+        public string Description { get; set; }
+        public DateTime LogTime { get; set; }
+        public int CardId { get; set; }
+        public virtual Card Card { get; set; }
+    }
+}
