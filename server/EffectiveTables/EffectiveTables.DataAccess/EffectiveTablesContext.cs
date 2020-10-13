792863489804d23ab383944eb9df5ed56b5c2a2a
Aleksandar <aca801@gmail.com>
2020-10-13 16:05:30 +0200

    Add DbContext

---
+++ server/EffectiveTables/EffectiveTables.DataAccess/EffectiveTablesContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EffectiveTables.DataAccess
+{
+    public class EffectiveTablesContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder builder)
+        {
+            base.OnConfiguring(builder);
+        }
+    }
+}
