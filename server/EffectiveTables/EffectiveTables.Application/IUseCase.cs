c9077bb9a08bde75b6a4b0feb9036faea92436a9
Aleksandar <aca801@gmail.com>
2020-10-13 14:32:33 +0200

    Create UseCase interface

---
+++ server/EffectiveTables/EffectiveTables.Application/IUseCase.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EffectiveTables.Application
+{
+    public interface IUseCase
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
