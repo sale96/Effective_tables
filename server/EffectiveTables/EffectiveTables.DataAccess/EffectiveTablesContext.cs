8159958af37f5336c640574d9cb77e8b00df12e6
sale96borca <aca801@gmail.com>
2020-10-26 12:52:25 +0100

    Update dbcontext to get connection string from environment variable

--- server/EffectiveTables/EffectiveTables.DataAccess/EffectiveTablesContext.cs
+++ server/EffectiveTables/EffectiveTables.DataAccess/EffectiveTablesContext.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
+using System.Runtime.InteropServices;
 using System.Text;
 
 namespace EffectiveTables.DataAccess
@@ -9,7 +10,19 @@ namespace EffectiveTables.DataAccess
     {
         protected override void OnConfiguring(DbContextOptionsBuilder builder)
         {
-            builder.UseSqlServer();
+            var connectionString = "";
+
+            if (
+                RuntimeInformation.IsOSPlatform(OSPlatform.Linux) || 
+                RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
+            {
+                connectionString = Environment.GetEnvironmentVariable("EFFECTIVE_TABLES_CONNECTION_UNIX");
+            } else if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+            {
+                connectionString = Environment.GetEnvironmentVariable("EFFECTIVE_TABLES_CONNECTION_WIN");
+            }
+            
+            builder.UseSqlServer(connectionString);
             base.OnConfiguring(builder);
         }
     }
