5b8030e17f43ccc32d757c96a5c2ce1b5431749f
sale96borca <aca801@gmail.com>
2020-10-26 15:27:44 +0100

    Change the way DbContex takes connection string

--- server/EffectiveTables.DataAccess/EffectiveTablesContext.cs
+++ server/EffectiveTables.DataAccess/EffectiveTablesContext.cs
@@ -1,8 +1,11 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 using System.Runtime.InteropServices;
 using System.Text;
+using EffectiveTables.Domain.Entities;
 
 namespace EffectiveTables.DataAccess
 {
@@ -10,21 +13,22 @@ namespace EffectiveTables.DataAccess
     {
         protected override void OnConfiguring(DbContextOptionsBuilder builder)
         {
-            var connectionString = "";
+            var connectionString = String.Empty;
 
-            if (
-                RuntimeInformation.IsOSPlatform(OSPlatform.Linux) || 
-                RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
-            {
-                connectionString = Environment.GetEnvironmentVariable("EFFECTIVE_TABLES_CONNECTION_UNIX");
-            }
-            else
-            {
-                connectionString = Environment.GetEnvironmentVariable("EFFECTIVE_TABLES_CONNECTION_WIN");
-            }
+            var open = File.ReadLines($@"{Directory.GetCurrentDirectory()}/Configuration/.env");
+
+            connectionString = open.First().Trim();
             
             builder.UseSqlServer(connectionString);
             base.OnConfiguring(builder);
         }
+        
+        public DbSet<Table> Tables { get; set; }
+        public DbSet<TableColumn> TableColumns { get; set; }
+        public DbSet<Card> Cards { get; set; }
+        public DbSet<Comment> Comments { get; set; }
+        public DbSet<CardLog> CardLogs { get; set; }
+        public DbSet<TableLog> TableLogs { get; set; }
+        public DbSet<CardLabel> CardLabels { get; set; }
     }
 }
