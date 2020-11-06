0e23648aa1086aa0e6fa0daa2cda432d99538e24
sale96borca <aca801@gmail.com>
2020-11-06 22:43:32 +0100

    Add empty table tests

---
+++ src/EffectiveTables.Tests/TableTests.cs
@@ -0,0 +1,30 @@
+using Xunit;
+
+namespace EffectiveTables.Tests
+{
+    public class TableTests
+    {
+        #region Crud
+
+        [Fact]
+        public void CreateTableTest()
+        {
+        }
+        
+        [Fact]
+        public void UpdateTableTest()
+        {
+        }
+
+        [Fact]
+        public void DeleteTableTest()
+        {
+        }
+
+        [Fact]
+        public void ReadTableTest()
+        {
+        }
+        #endregion
+    }
+}
